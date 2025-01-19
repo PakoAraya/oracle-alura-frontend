81b6e796e13dd7b3ccc6869a6d65a6a54c04da82
franarayah <franarayah@gmail.com>
2025-01-19 19:47:04 -0300

    Se estudian las condiciones if

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,11 @@
 
-alert ('Hola Mundo');
+let numeroSecreto = 7;
 let numeroUsuario = prompt("Me indicas un numero por favor:");
 
+console.log(numeroUsuario);
+
+if (numeroSecreto == numeroUsuario){
+    alert("¡Felicidades! ¡Has conseguido el número secreto!");
+}else{
+    alert("¡No has conseguido el número secreto!");
+}
