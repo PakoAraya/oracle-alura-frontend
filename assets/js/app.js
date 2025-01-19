ba75301ded732a1483b8efcf60ea790bbb95fd01
franarayah <franarayah@gmail.com>
2025-01-19 20:32:49 -0300

    Se estudian las condiciones else ademas del uso de templates string

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,13 @@ let numeroUsuario = prompt("Me indicas un numero por favor:");
 
 console.log(numeroUsuario);
 
+/*
+ * Este es un bloque de codigo que refleja comentarios
+ * en el código fuente...
+ */
+
 if (numeroSecreto == numeroUsuario){
-    alert("¡Felicidades! ¡Has conseguido el número secreto!");
+    alert(`¡Felicidades! El numero correcto es: ${numeroSecreto}`);
 }else{
-    alert("¡No has conseguido el número secreto!");
+    alert(`¡No has conseguido el número secreto!, el numero correcto era: ${numeroSecreto}`);
 }
