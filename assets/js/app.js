a55bb01bb8c9cfef7f2147f0d209b09fe0f742f3
franarayah <franarayah@gmail.com>
2025-01-20 18:21:17 -0300

    Se estudia los metodos random y floor de la clase math en javascript

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,15 @@
 
-let numeroSecreto = 7;
+let numeroSecreto = Math.floor(Math.random() * 10) + 1;
 let numeroUsuario = 0;
 let intentos =1;
 //let palabraVeces = 'vez';
 let maximoIntentos = 3;
+
+console.log(numeroSecreto);
+
 while(numeroUsuario != numeroSecreto){
-  numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
-  console.log(numeroUsuario);
+  numeroUsuario = parseInt(prompt("Me indicas un numero entre 1 y 10 por favor:"));
+  console.log(typeof(numeroUsuario));
 
   /*
    * Este es un bloque de codigo que refleja comentarios
