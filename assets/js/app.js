b4105932584aaf79f8255d82c988168686cdaee8
franarayah <franarayah@gmail.com>
2025-01-20 12:38:21 -0300

    se estudia operador ternario y se mejoran los string templates

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,8 @@
 let numeroSecreto = 7;
 let numeroUsuario = 0;
 let intentos =1;
-let palabraVeces = 'vez';
+//let palabraVeces = 'vez';
+let maximoIntentos = 3;
 while(numeroUsuario != numeroSecreto){
   numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
   console.log(numeroUsuario);
@@ -12,18 +13,18 @@ while(numeroUsuario != numeroSecreto){
    * en el código fuente...
    */
 
-  if (numeroSecreto == numeroUsuario){
-    alert(`¡Felicidades! El numero correcto es: ${numeroSecreto}. Lo hiciste en ${intentos} ${palabraVeces}.`);
+  if (numeroUsuario == numeroSecreto){
+    alert(`¡Felicidades! El numero correcto es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez': 'veces'}`);
   }else{
-    if(numeroSecreto > numeroUsuario){
-      alert(`El numero secreto es menor: ${numeroSecreto}. Lo hiciste en ${intentos} ${palabraVeces}.`);
+    if(numeroUsuario > numeroSecreto){
+      alert(`El numero secreto es menor`);
     }else{
-      alert(`El numero secreto es mayor: ${numeroSecreto}. Lo hiciste en ${intentos} ${palabraVeces}.`);
+      alert(`El numero secreto es mayor`);
     }
     intentos++;
     palabraVeces = 'veces';
     if(intentos > 3){
-      alert('Llegaste al numero maximo de intentos');
+      alert(`Llegaste al numero maximo de ${maximoIntentos} intentos.`);
       break;
     }
   }
