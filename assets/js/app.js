24581b6dd0940ea0071277592db21427c4875b28
franarayah <franarayah@gmail.com>
2025-01-19 23:34:10 -0300

    Se estudia el ciclo while en javascript

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,22 @@
 
 let numeroSecreto = 7;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+let numeroUsuario = 0;
+while(numeroUsuario != numeroSecreto){
+  numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
+  console.log(numeroUsuario);
 
-console.log(numeroUsuario);
+  /*
+   * Este es un bloque de codigo que refleja comentarios
+   * en el código fuente...
+   */
 
-/*
- * Este es un bloque de codigo que refleja comentarios
- * en el código fuente...
- */
-
-if (numeroSecreto == numeroUsuario){
-  alert(`¡Felicidades! El numero correcto es: ${numeroSecreto}`);
-}else{
-  if(numeroSecreto > numeroUsuario){
-    alert(`El numero secreto es menor: ${numeroSecreto}`);
+  if (numeroSecreto == numeroUsuario){
+    alert(`¡Felicidades! El numero correcto es: ${numeroSecreto}`);
   }else{
-    alert(`El numero secreto es mayor: ${numeroSecreto}`);
+    if(numeroSecreto > numeroUsuario){
+      alert(`El numero secreto es menor: ${numeroSecreto}`);
+    }else{
+      alert(`El numero secreto es mayor: ${numeroSecreto}`);
+    }
   }
 }
