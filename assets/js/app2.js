25af06660ddbddfd275583e1f488c9a02e33544e
franarayah <franarayah@gmail.com>
2025-01-21 18:07:20 -0300

    Se genera numero rando y se asocia una funcion a una variable

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -1,10 +1,21 @@
 
+let numeroSecreto = generarNumeroSecreto();
+
+console.log(numeroSecreto);
+
 function asignarTextoElemento(elemento, texto){
   let elementoHTML = document.querySelector(elemento);
   elementoHTML.innerHTML = texto;
+  return;
 }
 function intentoDeUsuario(){
   alert('Click en el boton intentar desde la funcion');
+  return;
+}
+
+function generarNumeroSecreto(){
+  return Math.floor(Math.random() * 100) + 1;
+
 }
 
 asignarTextoElemento('h1', 'Juego del número secreto');
