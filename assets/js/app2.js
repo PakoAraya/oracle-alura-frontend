3a6c55cf4196e803c082bf633f2cfb0700ea8a78
franarayah <franarayah@gmail.com>
2025-01-21 14:18:38 -0300

    Se estudia el query selector de javascript

---
+++ assets/js/app2.js
@@ -0,0 +1,7 @@
+
+let titulo = document.querySelector('h1');
+titulo.innerHTML = '¡Juego del Número Secreto!';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Escribe un número entre 1 y 10';
+
