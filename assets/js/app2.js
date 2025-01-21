6f7c70402467c1111cb4c583aef803260f48b977
franarayah <franarayah@gmail.com>
2025-01-21 14:51:07 -0300

    Se estudia eventos on click y llamada de funciones

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -5,3 +5,6 @@ titulo.innerHTML = '¡Juego del Número Secreto!';
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Escribe un número entre 1 y 10';
 
+function intentoDeUsuario(){
+  alert('Click en el boton intentar desde la funcion');
+}
