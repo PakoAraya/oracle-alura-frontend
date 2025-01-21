972e6ce58e55c0312284e34eb663d2eba57cf213
franarayah <franarayah@gmail.com>
2025-01-21 18:22:48 -0300

    Se estudia la captura de inputs y validacion de datos de mismo tipo

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -1,21 +1,23 @@
 
 let numeroSecreto = generarNumeroSecreto();
 
-console.log(numeroSecreto);
-
 function asignarTextoElemento(elemento, texto){
   let elementoHTML = document.querySelector(elemento);
   elementoHTML.innerHTML = texto;
   return;
 }
-function intentoDeUsuario(){
-  alert('Click en el boton intentar desde la funcion');
+function verificarIntento(){
+  let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+  console.log(typeof(numeroDeUsuario));
+  console.log(numeroSecreto);
+  console.log(typeof(numeroSecreto));
+  console.log(numeroDeUsuario);
+  console.log(numeroDeUsuario === numeroSecreto);
   return;
 }
 
 function generarNumeroSecreto(){
   return Math.floor(Math.random() * 100) + 1;
-
 }
 
 asignarTextoElemento('h1', 'Juego del número secreto');
