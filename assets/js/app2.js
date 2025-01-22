0b2410de88e97246cdccf70bb61656ab87d279fe
franarayah <franarayah@gmail.com>
2025-01-22 12:58:18 -0300

    Se optimiza el codigo y se crean funciones que optimizan funcionamiento para limpiar campo y reiniciar juego

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -1,6 +1,6 @@
 
-let numeroSecreto = generarNumeroSecreto();
-let intentos = 1;
+let numeroSecreto = 0;
+let intentos = 0;
 
 function asignarTextoElemento(elemento, texto){
   let elementoHTML = document.querySelector(elemento);
@@ -13,20 +13,45 @@ function verificarIntento(){
   console.log(numeroSecreto);
   if(numeroDeUsuario === numeroSecreto) {
     asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez': 'veces'}`);
+    document.getElementById('reiniciar').disabled = false;
   }else if(numeroDeUsuario > numeroSecreto){
     asignarTextoElemento('p', 'El número secreto es menor');
   }else{
     asignarTextoElemento('p', 'El número secreto es mayor');
   }
   intentos++;
-  
+  limpiarCaja();
+
   return;
 }
 
-function generarNumeroSecreto(){
-  return Math.floor(Math.random() * 100) + 1;
+function limpiarCaja(){
+  document.querySelector('#valorUsuario').value = '';
 }
 
-asignarTextoElemento('h1', 'Juego del número secreto');
-asignarTextoElemento('p', 'Escribe un número entre 1 y 100');
+function generarNumeroSecreto(){
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+function condicionesIniciales(){
+  asignarTextoElemento('h1', 'Juego del número secreto');
+  asignarTextoElemento('p', 'Escribe un número entre 1 y 100');
+  numeroSecreto = generarNumeroSecreto();
+  intentos = 1;
+}
+
+function reiniciarJuego(){
+  //Limpiar caja de texto
+  limpiarCaja();
+  //Indicar mensajes de intervalo de numeros
+  //Generar nuevo número aleatorio
+  // Reiniciar numero de intentos
+  condicionesIniciales();
+  //Deshabilitar el boton de nuevo juego
+  document.querySelector('#reiniciar').setAttribute('disabled', true);
+}
+
+condicionesIniciales();
+
+
 
